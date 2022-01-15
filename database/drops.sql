70431686bf2d5f39761db565dca157b3c7a06525
Gianlucca Aguado <gianlucca.aguado@jikkosoft.com>
2022-01-15 00:36:24 -0500

    ENDPOINT: /employees/{id} PATH (UpdateEmployees)
    - validaciones correspondientes probadas
    - statuscodes response probados
    - procedimiento contiene la mayor logica
    - Endpoint Finalizado
    
    -- Add drops

--- database/drops.sql
+++ database/drops.sql
@@ -13,3 +13,13 @@ drop type if exists status_user;
 drop function if exists insert_employees(uuid, varchar, varchar, varchar, varchar, integer, integer, varchar, varchar,
     varchar, varchar, integer) cascade;
 
+drop function if exists select_employees(varchar, character varying[], character varying[], character varying[],
+    varchar, varchar, integer) cascade;
+
+drop function if exists update_employee(uuid, varchar, varchar, varchar, varchar, integer, integer, varchar, integer,
+    status_user, uuid) cascade;
+
+
+
+
+
